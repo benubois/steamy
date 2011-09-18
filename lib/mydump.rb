987a33b1574304a4cd1d3c45fb633933f8485d37
Ben Ubois <bb@flickerbox.com>
2011-09-18 11:22:30 -0700

    Added database listing. Updated commands.

--- lib/mydump.rb
+++ lib/mydump.rb
@@ -6,7 +6,7 @@ require "pp"
 module Mydump
   class Mydump
     
-    def initialize(host)
+    def initialize(host, database = nil)
       @settings = Config.config
       @settings[:saved_connections] = File.expand_path(@settings[:saved_connections])
       @host = host
@@ -14,24 +14,49 @@ module Mydump
       sequel_pro = SequelPro.new(@settings)
       @connections = sequel_pro.connections
       
-      @ssh = "ssh #{@connections[@host]['ssh_host']}"
+      @ssh      = "ssh #{@connections[@host]['ssh_host']}"
+      @user     = (@connections[@host]['user'].nil?) ? '' :  "-u #{@connections[@host]['user']}"
+      @password = (@connections[@host]['password'].nil?) ? '' :  "-p#{@connections[@host]['password']}"
+      @database = database
     end
     
     def remote_exec(command)
-      puts `#{@ssh} '#{command}'`
+      # puts "#{@ssh} #{command}"
+      `#{@ssh} '#{command}'`
     end
     
     def show_databases
-      remote_exec('mysql -e "show databases" -u ' + @connections[@host]['user'] + ' -p' + @connections[@host]['password'])
+      remote_exec(sprintf('\'mysql -e "show databases" %s %s\'', @user, @password))
+    end
+    
+    def get_database
+      databases = show_databases.split("\n")
+      
+      pp databases
+
+      puts "Choose a database to dump:"
+      databases.each_with_index do |db, i|
+        unless 
+          puts i.to_s + ': ' + db
+        end
+      end
+
+      # Get index from user
+      index = gets.to_i
+      databases[index]
     end
     
     def mysqldump
-      remote_exec("mysqldump -u #{@connections[@host]['user']} -p#{@connections[@host]['password']} #{@database} | gzip -c")
+      if @database.nil?
+        @database = get_database
+      end
+      remote_exec(sprintf("mysqldump '%s %s %s | gzip -c' > ~/Desktop/db.sql", @user, @password, @database))
     end
+    
   end
 end
 
 
 
-preview = Mydump::Mydump.new('')
-preview.show_databases
+preview = Mydump::Mydump.new('', 'mysql')
+preview.mysqldump
